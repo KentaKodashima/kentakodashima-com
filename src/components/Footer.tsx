68835f02b36e65d3dfc322d513a9f68f3c78c64e
Kenta Kodashima <kentakodashima@gmail.com>
2019-10-15 23:34:50 -0700

    Styling the IndexPage.
    TODO:
     - Make Chevrons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,12 @@
-import React from "react"
+import React from 'react'
 
-import Container from "./Container"
+import footerStyle from '../scss/components/Footer.module.scss'
+import Container from './Container'
 
 const Footer = () => {
   return (
-    <footer>
-      <Container>
-        © {new Date().getFullYear()} Kenta Kodashima
-      </Container>
+    <footer className={footerStyle.footer}>
+      <Container>© {new Date().getFullYear()} Kenta Kodashima</Container>
     </footer>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,22 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
 
-import indexPageStyle from "../scss/pages/IndexPage.module.scss"
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import indexPageStyle from '../scss/pages/IndexPage.module.scss'
+import Layout from '../components/layout'
+import Image from '../components/image'
+import SEO from '../components/seo'
+import Container from '../components/Container'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h2>Kenta Kodashima</h2>
-    <h3>Full-stack Developer</h3>
-    <div 
-      className={indexPageStyle.heroImage}
-    >
+    <div className={indexPageStyle.heroText}>
+      <h2>Kenta Kodashima</h2>
+      <h3>Full-stack Developer</h3>
+    </div>
+    <div className={indexPageStyle.heroImage}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
