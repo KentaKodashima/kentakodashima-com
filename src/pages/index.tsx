8c21ac5021736b60ab426c1fb30dc86dfce5d64a
Kenta Kodashima <kentakodashima@gmail.com>
2019-11-03 22:35:30 -0800

    Fix the typos in the pages.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import indexPageStyle from '../scss/pages/IndexPage.module.scss'
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 import Image from '../components/image'
 import SEO from '../components/seo'
 import CatImg from '../images/hero-cat.svg'
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import projectsPageStyles from '../scss/pages/ProjectsPage.module.scss'
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 import SEO from '../components/seo'
 import ProjectList from '../components/ProjectList'
 import { ChevronsContext } from '../themes/themeContext'
