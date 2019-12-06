ed2aa8b0aeb020e7b43951b8cac7376d94a5e2f0
Kenta Kodashima <kentakodashima@gmail.com>
2019-12-06 08:26:58 -0800

    Modify the config file.
    TODO:
     - Animations
     - URL parameter
     - Image (list) query

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,8 +5,33 @@
  */
 
 // You can delete this file if you're not using it
+
+// Fix: https://github.com/gatsbyjs/gatsby/issues/4753
 const { fmImagesToRelative } = require('gatsby-remark-relative-images')
 
 exports.onCreateNode = ({ node }) => {
+  const { createNodeField } = actions
+  const { frontmatter } = node
+  if (frontmatter) {
+    const { image } = frontmatter
+    if (image) {
+      if (image.indexOf('/images') === 0) {
+        frontmatter.image = path.relative(
+          path.dirname(node.fileAbsolutePath),
+          path.join(__dirname, '/static/assets/', image)
+        )
+      }
+    }
+  }
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+
   fmImagesToRelative(node)
 }
