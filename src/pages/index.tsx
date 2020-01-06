7452868c578fa4b345f7140bfd5d999806803f21
Kenta Kodashima <kentakodashima@gmail.com>
2020-01-05 21:52:43 -0800

    Fix the vh issue on mobile.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,11 +13,6 @@ const IndexPage = props => {
     // Fix for 100vh on mobile browsers
     let vh = window.innerHeight * 0.01
     document.documentElement.style.setProperty('--vh', `${vh}px`)
-
-    window.addEventListener('resize', () => {
-      let vh = window.innerHeight * 0.01
-      document.documentElement.style.setProperty('--vh', `${vh}px`)
-    })
   })
 
   return (
