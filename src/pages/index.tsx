bcecbd4d438c81931b89a7f19d5d95c15e38d67c
Kenta Kodashima <kentakodashima@gmail.com>
2020-01-05 21:35:07 -0800

    Add the code to fix vh problems on mobile.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,9 @@ import { ChevronsContext, PaddingTopContext } from '../themes/contexts'
 const IndexPage = props => {
   useEffect(() => {
     // Fix for 100vh on mobile browsers
+    let vh = window.innerHeight * 0.01
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
+
     window.addEventListener('resize', () => {
       let vh = window.innerHeight * 0.01
       document.documentElement.style.setProperty('--vh', `${vh}px`)
