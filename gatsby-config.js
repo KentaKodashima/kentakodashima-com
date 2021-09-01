b2a716cf776059546234cbdc4f7218ba12e669ca
Kenta Kodashima <kentakodashima@gmail.com>
2021-08-31 17:12:06 -0700

    Modify the OG image configuration to use proper image path.

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
     description: `This is the portfolio website of Kenta Kodashima, a full-stack developer`,
     author: `Kenta Kodashima`,
     url: `https://kentakodashima.com`,
-    image: 'src/images/ogimage.png'
+    image: `${__dirname}/src/images/ogimage.png`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
